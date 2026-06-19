c1c6142a9a9ea50e760d88029f9b39b908b6960e
Gilang Ramdani <gilangramdanibusines@gmail.com>
2026-06-20 02:04:25 +0700

    fix render

--- index.js
+++ index.js
@@ -7,3 +7,16 @@
  */
 
 require("./src/bot");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
